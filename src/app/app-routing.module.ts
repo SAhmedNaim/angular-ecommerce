50ef9fa12321a848b4527fe600d7140ce422d617
SAhmedNaim <naim.ahmed035@gmail.com>
2023-02-20 23:39:29 +0600

    Introducing Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'seller-auth',
+    component: SellerAuthComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
